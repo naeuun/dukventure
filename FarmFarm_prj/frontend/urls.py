878b765ea32b05a8e29f37f04407050f58ef93d9
허윤아 <aquahua@gmail.com>
2025-08-01 14:36:10 +0900

    [0801] 판매자 온보딩 페이지 초안, 음성인식기능 미개발 상태

--- FarmFarm_prj/frontend/urls.py
+++ FarmFarm_prj/frontend/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path("example", example, name="example"),
+    path("selleronboarding1", selleronboarding1, name="selleronboarding1"),
+    path("selleronboarding2", selleronboarding2, name="selleronboarding2"),
+    path("selleronboarding3", selleronboarding3, name="selleronboarding3"),
+    path("selleronboarding4", selleronboarding4, name="selleronboarding4"),
+    path("selleronboarding5", selleronboarding5, name="selleronboarding5"),
 ]
--- FarmFarm_prj/frontend/views.py
+++ FarmFarm_prj/frontend/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def example(request):
     return(render(request, "example.html"))
+
+def selleronboarding1(request):
+    return render(request, "seller_onboardings/seller_onboarding_1.html")
+def selleronboarding2(request):
+    return render(request, "seller_onboardings/seller_onboarding_2.html")
+def selleronboarding3(request):
+    return render(request, "seller_onboardings/seller_onboarding_3.html")
+def selleronboarding4(request):
+    return render(request, "seller_onboardings/seller_onboarding_4.html")
+def selleronboarding5(request):
+    return render(request, "seller_onboardings/seller_onboarding_5.html")
