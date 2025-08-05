7a6553bf2de0d884b779998ab517f9c964b7a428
naeun <1210naeun@gmail.com>
2025-08-05 19:47:54 +0900

    chore(BE): gitignore, 세팅.py 앱 추가

--- FarmFarm_prj/FarmFarm_prj/settings.py
+++ FarmFarm_prj/FarmFarm_prj/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'store',
+    'items',
+    'users',
+    'reservations',
+    'shopping',
+    'reviews',
 ]
 
 MIDDLEWARE = [
