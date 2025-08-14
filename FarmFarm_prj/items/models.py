2d9ad2118d8013447b32def9b028ec356d0a3b90
naeun <1210naeun@gmail.com>
2025-08-15 05:49:30 +0900

    fix(BE): 오류 1차 해결

--- FarmFarm_prj/items/models.py
+++ FarmFarm_prj/items/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
 class Item(models.Model):
-<<<<<<< HEAD
-    name = models.CharField(max_length=100)
-    price = models.PositiveIntegerField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    unit = models.CharField(max_length=20, blank=True, help_text='단위(봉, 단, kg 등)') #추가
-=======
     name = models.CharField(max_length=100, verbose_name='품목명')
     description = models.TextField(blank=True, null=True, verbose_name='설명')
->>>>>>> backend
+    unit = models.CharField(max_length=20, blank=True, help_text='단위(봉, 단, kg 등)', verbose_name='단위') #추가
 
     def __str__(self):
         return self.name
