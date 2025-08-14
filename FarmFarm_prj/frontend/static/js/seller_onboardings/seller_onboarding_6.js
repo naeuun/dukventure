d5848e547cf4ca93421dcff49efcc7934a7b54ae
허윤아 <aquahua@gmail.com>
2025-08-14 14:25:24 +0900

    refactor(FE): 온보딩 6 페이지 프로그래스바 수정

--- FarmFarm_prj/frontend/static/js/seller_onboardings/seller_onboarding_6.js
+++ FarmFarm_prj/frontend/static/js/seller_onboardings/seller_onboarding_6.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const btn = document.getElementById("verify");
   const text = document.getElementById("text");
+  let bar = document.getElementsByClassName("now_step")[0];
   let cnt = 0;
 
   btn.addEventListener("click", function () {
@@ -8,8 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (cnt === 1) {
       text.innerHTML = "인증완료";
+      bar.style.width = "290.5px";
     } else if (cnt === 2) {
       location.href = "selleronboarding5";
     }
   });
+  
 });
